ee9d145b8f200cabb47dba99922b4b240380cc90
ameengee <ameen.gee@gmail.com>
2024-12-01 13:25:28 -0800

    test3

--- app/components/chat/Header.js
+++ app/components/chat/Header.js
@@ -9,7 +9,12 @@ import { addChat } from '@/app/redux/slices/asstListSlice'
 import { newChatHistory } from '@/app/redux/slices/currentChatSlice';
 import { exitEditMode } from '@/app/redux/slices/editAsstSlice';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { API } from 'aws-amplify';
+
 const Header = () => {
+  Amplify.configure(awsconfig);
   const currentAssisstant = useSelector(state => state.asstList.currentAssisstant);
   const currentChat = useSelector(state => state.asstList.currentChat);
 
@@ -25,6 +30,15 @@ const Header = () => {
     dispatch(addChat(currentAssisstant));
     dispatch(newChatHistory());
     dispatch(exitEditMode());
+  }
+
+  const handleLoginPress = async () => {
+    try {
+      const response = await API.get('amplify-test-api', '/on-user-message'); // Replace with your resource path
+      console.log('Lambda Response:', response);
+    } catch (error) {
+      console.error('Error calling Lambda:', error);
+    }
   }
 
   return (
@@ -44,7 +58,7 @@ const Header = () => {
         <h2 className='assisstant-name'>{`${currentAssisstant} - ${currentChat}`}</h2>
       </div>
       <div className='login-btn-container'>
-        <button className='login-btn'>Login</button>
+        <button className='login-btn' onClick={handleLoginPress}>Login</button>
       </div>
     </div>
   );
