dd6125886b1642e4e5dac7be6ba4b90e6a416c08
Alexander Anokhin <alexander.g.anokhin@gmail.com>
2024-12-04 10:32:08 -0800

    test with cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "crawlaiappagain9c520872": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
